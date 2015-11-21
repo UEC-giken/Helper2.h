28e5dc706818e5c0b9286bf71756790906ae0f46
kyontan <kyontan@monora.me>
2015-11-21 17:35:43 +0900

    Accel: update example

--- examples/fade/fade.ino
+++ examples/fade/fade.ino
@@ -1,37 +1,24 @@
 #include <Helper2.h>
 #include <Wire.h>
 
-bool tapped;
-
 void setup() {
   initialize();
-  
+
   led1.randomcolor();
-  led2.randomcolor();  
+  led2.randomcolor();
   led1.on();
   led2.on();
 }
 
 void loop() {
-  if (tapped){
-    led1.debug_print();
+  if (accel.active()) {
     led1.fadeIn();
-    led2.debug_print();
     led2.fadeOut();
-    if (accel.tap()){
-      tapped = false;
-    }
-  }
-  else {
-    led1.debug_print();
+  } else {
     led1.fadeOut();
-    led2.debug_print();
-    led2.fadeIn(); 
-    if (accel.tap()){
-      tapped = true;
-    }
+    led2.fadeIn();
   }
-  
-  wait(100);
+
+  wait(50);
 }
 
